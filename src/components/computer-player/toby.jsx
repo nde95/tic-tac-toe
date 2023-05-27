a8288cdcbe57b11b8e4850e72883e754ffd82bc9
nde95 <ndennison268@gmail.com>
2023-05-27 11:19:12 -0400

    stopping toby's free moves after X wins

--- src/components/computer-player/toby.jsx
+++ src/components/computer-player/toby.jsx
@@ -1,12 +1,36 @@
 import { useContext, useEffect } from "react";
 import { GameContext } from "../../context/gamecontext";
+import useGameOver from "../game-over-alert/game-over-alert.component";
 
 const Toby = ({ currentPlayer, setCurrentPlayer }) => {
-    const {gameState, setGameState, gameOver, setGameOver} = useContext(GameContext);
+    const {gameState, setGameState, gameOver, setGameOver, checkDraw, checkWin, resetGame} = useContext(GameContext);
+    const { handleWin, handleDraw } = useGameOver(currentPlayer, setCurrentPlayer, resetGame);
+
+    useEffect(() => {
+      if (checkWin(currentPlayer)) {
+        setGameOver(true)
+        handleWin();
+      } else if (checkDraw()) {
+        setGameOver(true)
+        handleDraw();
+      }
+    }, [currentPlayer]);
 
     const makeTobyMove = () => {
+
+        if (currentPlayer !== 'O' || gameOver) {
+            return;
+        }
+
         // Check if it's PlayerO's turn
-        if (currentPlayer === 'O') {
+         else if (currentPlayer === 'O') {
+            // If X has won or the game has been drawn, don't make a move
+            if (checkWin('X') || checkDraw()) {
+                // pull the alert and force turn back to X to disable game board
+                handleWin();
+                setCurrentPlayer('X')
+                return;
+            }
           // Get the indices of all empty squares
           const emptySquares = [];
           gameState.forEach((row, rowIndex) => {
@@ -39,16 +63,30 @@ const Toby = ({ currentPlayer, setCurrentPlayer }) => {
               return cell;
             })
           );
-          setGameState(updatedGameState);
 
-          // switch turn to X after making a move
-          setCurrentPlayer('X');
+           // Check if Toby has won after making his move
+            if (checkWin('O', updatedGameState)) {
+                setGameState(updatedGameState);
+                setGameOver(true);
+                handleWin();
+                return; // Exit the function as the game is over
+            } else if (checkDraw(updatedGameState)) {
+                setGameState(updatedGameState);
+                setGameOver(true);
+                handleDraw();
+                return; // Exit the function as the game is over
+            }
+
+            // If Toby hasn't won and the game is not a draw, update the gameState and switch turn to X
+            setGameState(updatedGameState);
+            setCurrentPlayer('X');
         }
       };
 
     useEffect(() => {
       makeTobyMove();
     }, [currentPlayer, gameOver]);
+
 
     return null; 
 }
