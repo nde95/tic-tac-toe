4442d1001b4faa8a60b8cf5179a0bc0be6fa18df
nde95 <ndennison268@gmail.com>
2023-05-27 09:31:51 -0400

    checking for matches within grids

--- src/context/gamecontext.jsx
+++ src/context/gamecontext.jsx
@@ -16,9 +16,33 @@ const GameProvider = ({ children }) => {
         setGameState(initialGameState);
         setGameOver(false);
       };
+
+      const checkWin = (player) => {
+        // Check rows
+        for (let row = 0; row < 3; row++) {
+          if (gameState[row][0] === player && gameState[row][1] === player && gameState[row][2] === player) {
+            return true;
+          }
+        }
+        // Check columns
+        for (let col = 0; col < 3; col++) {
+          if (gameState[0][col] === player && gameState[1][col] === player && gameState[2][col] === player) {
+            return true;
+          }
+        }
+        // Check diagonals
+        if (gameState[0][0] === player && gameState[1][1] === player && gameState[2][2] === player) {
+          return true;
+        }
+        if (gameState[2][0] === player && gameState[1][1] === player && gameState[0][2] === player) {
+          return true;
+        }
+        return false;
+      };
+      
     
       return (
-        <GameContext.Provider value={{gameState, setGameState, gameOver, resetGame, setGameOver}}>
+        <GameContext.Provider value={{gameState, setGameState, gameOver, resetGame, setGameOver, checkWin}}>
           {children}
         </GameContext.Provider>
       );
