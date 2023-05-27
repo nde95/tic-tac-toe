902a10043cc978dcbbccb1ce445c0033616482b4
nde95 <ndennison268@gmail.com>
2023-05-27 11:19:24 -0400

    moving alert to its own component

---
+++ src/components/game-over-alert/game-over-alert.component.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+
+const useGameOver = (currentPlayer, setCurrentPlayer, resetGame) => {
+  const [gameOver, setGameOver] = useState(false);
+
+  const handleWin = () => {
+    setGameOver(true);
+    Swal.fire({
+      title: currentPlayer + ' wins!',
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: 'Let Toby go first!',
+      denyButtonText: `I want to go first!`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        resetGame();
+        setCurrentPlayer('O');
+      } else if (result.isDenied) {
+        setCurrentPlayer('X');
+        resetGame();
+      }
+    });
+  };
+
+  const handleDraw = () => {
+    setGameOver(true);
+    Swal.fire({
+      title: 'It\'s a draw!',
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: 'Let Toby go first!',
+      denyButtonText: `I want to go first!`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        resetGame();
+      } else if (result.isDenied) {
+        setCurrentPlayer('X');
+        resetGame();
+      }
+    });
+  };
+
+  return { gameOver, handleWin, handleDraw };
+};
+
+export default useGameOver;
